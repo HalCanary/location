44acda622e15e7f4b1a9d93afd99c42ec9588bf7
Hal Canary <halcanary@gmail.com>
2024-07-07 16:53:08 -0400

    update

--- Location.m
+++ Location.m
@@ -5,29 +5,36 @@
 #include <stdlib.h>
 #include <AppKit/AppKit.h>
 #include <CoreLocation/CoreLocation.h>
-@interface Delegate : NSObject <CLLocationManagerDelegate>
+@interface Delegate : NSObject <CLLocationManagerDelegate, NSApplicationDelegate>
     @property (strong) CLLocationManager* mgr;
+    @property int errorcount;
 @end
 @implementation Delegate
     - (instancetype) init {
-        [self setMgr:[[CLLocationManager alloc] init]];
-        [self.mgr setDelegate:self];
-        [self.mgr setDesiredAccuracy:kCLLocationAccuracyBest];
-        [self.mgr setDistanceFilter:2.0];
-        [self.mgr requestAlwaysAuthorization];
-        [self.mgr startUpdatingLocation];
+        self.errorcount = 0;
+        [[NSApplication sharedApplication] setDelegate:self];
         return self;
     }
     - (void)locationManager:(CLLocationManager*)m didFailWithError:(NSError*)e {
         fprintf(stderr, "CLLocationManager error: %s\n",
             [[e localizedDescription] UTF8String]);
-        exit(1);
+        if (self.errorcount++ > 1) {
+            exit(1);
+        }
     }
     - (void) locationManager:(CLLocationManager*)m didUpdateLocations:(NSArray<CLLocation*>*)ls {
         for (CLLocation* l in ls) {
             printf("%f, %f", l.coordinate.latitude, l.coordinate.longitude);
         }
         [NSApp terminate:self];
+    }
+    - (void)applicationDidFinishLaunching:(NSNotification*)n {
+        [self setMgr:[[CLLocationManager alloc] init]];
+        [self.mgr setDelegate:self];
+        [self.mgr setDesiredAccuracy:kCLLocationAccuracyBest];
+        [self.mgr setDistanceFilter:2.0];
+        [self.mgr requestAlwaysAuthorization];
+        [self.mgr startUpdatingLocation];
     }
     - (void)locationManager:(CLLocationManager*)m
             didUpdateToLocation:(CLLocation*)l
--- Makefile
+++ Makefile
@@ -1,18 +1,32 @@
 
 Location: Location.m Location.info.plist
 	cc $< \
-		-framework CoreLocation  \
-		-framework AppKit  \
-		-sectcreate __TEXT __info_plist Location.info.plist \
-		-o $@
-	if [ "$$CODESIGN_IDENTITY" ]; then \
-		codesign \
-			-s "${CODESIGN_IDENTITY}" \
-			--verbose --force \
-			--options runtime $@ ; fi
+	  -framework CoreLocation  \
+	  -framework AppKit  \
+	  -sectcreate __TEXT __info_plist Location.info.plist \
+	  -o $@
+
+codesign.stamp: Location
+	codesign \
+	  -s ${CODESIGN_IDENTITY} \
+	  --verbose --force \
+	  --options runtime $<
+	codesign -d --entitlements /dev/stdout $< 
+	touch $@
 
 run: Location 
 	./Location
+
+Location.zip: Location codesign.stamp
+	zip $@ $^
+
+notarize.stamp: Location.zip
+	xcrun notarytool submit $^ \
+	  --wait \
+	  --apple-id ${APPLE_ID} \
+	  --team-id  ${CODESIGN_IDENTITY} \
+	  --password ${CODESIGN_PASSWORD}
+	touch $@
 
 clean:
 	rm Location
